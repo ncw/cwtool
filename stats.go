b05bbfa98dc45e684f1e9370c70584f307e34a76
Nick Craig-Wood <nick@craig-wood.com>
2021-09-25 18:50:53 +0100

    Add percentiles to stats summary

--- stats.go
+++ stats.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
 	"sort"
 	"strconv"
@@ -12,21 +13,90 @@ import (
 	"time"
 )
 
+// Hold stats about a letter or group of letters
+type Stat struct {
+	Name          string
+	ReactionTimes []float64
+	Errors        int
+	Total         int
+	ConfusedWith  []string
+
+	sorted bool
+	sum    float64 // sum of ReactionTimes
+}
+
+// NewStat instantiates a new statistic
+func NewStat(Name string) *Stat {
+	s := &Stat{
+		Name: Name,
+	}
+	return s
+}
+
+func (s *Stat) Add(tx, rx string, reactionTime float64) {
+	s.sorted = false
+	if rx == tx {
+		s.ReactionTimes = append(s.ReactionTimes, reactionTime)
+		s.sum += reactionTime
+	} else {
+		s.Errors++
+		s.ConfusedWith = append(s.ConfusedWith, string(rx))
+	}
+	s.Total++
+}
+
+func (s *Stat) sort() {
+	if !s.sorted {
+		sort.Float64s(s.ReactionTimes)
+		s.sorted = true
+	}
+}
+
+func (s *Stat) Min() float64 {
+	if len(s.ReactionTimes) == 0 {
+		return 0
+	}
+	s.sort()
+	return s.ReactionTimes[0]
+}
+
+func (s *Stat) Max() float64 {
+	if len(s.ReactionTimes) == 0 {
+		return 0
+	}
+	s.sort()
+	return s.ReactionTimes[len(s.ReactionTimes)-1]
+}
+
+func (s *Stat) Avg() float64 {
+	if len(s.ReactionTimes) == 0 {
+		return 0
+	}
+	return s.sum / float64(len(s.ReactionTimes))
+}
+
+func (s *Stat) Percentile(p float64) float64 {
+	if len(s.ReactionTimes) == 0 {
+		return 0
+	}
+	s.sort()
+	i := int(math.Round((p / 100) * float64(len(s.ReactionTimes))))
+	return s.ReactionTimes[i]
+}
+
+func (s *Stat) PercentageErrors() float64 {
+	if s.Total == 0 {
+		return 0
+	}
+	return float64(s.Errors) / float64(s.Total) * 100.0
+}
+
 type Stats struct {
 	StatByLetter map[string]*Stat
+	Total        *Stat
 
 	// Working variables - not serialized
 	csvFile string
-}
-
-type Stat struct {
-	Letter        string
-	ReactionTimes []float64
-	Errors        int
-	ConfusedWith  []string
-
-	// Working variables - not serialized
-	min, avg, max float64
 }
 
 // NewStats loads the stats from the fileName if found otherwise
@@ -35,6 +105,7 @@ func NewStats(csvFile string) *Stats {
 	s := &Stats{
 		csvFile:      csvFile,
 		StatByLetter: map[string]*Stat{},
+		Total:        NewStat("total"),
 	}
 	s.Load()
 	return s
@@ -44,59 +115,11 @@ func (s *Stats) Add(tx, rx string, reactionTime float64) {
 	letter := string(tx)
 	stat := s.StatByLetter[letter]
 	if stat == nil {
-		stat = new(Stat)
+		stat = NewStat(letter)
 		s.StatByLetter[letter] = stat
-		stat.Letter = letter
 	}
-	stat.ReactionTimes = append(stat.ReactionTimes, reactionTime)
-	if rx != tx {
-		stat.Errors++
-		stat.ConfusedWith = append(stat.ConfusedWith, string(rx))
-	}
-}
-
-func min(xs []float64) float64 {
-	if len(xs) == 0 {
-		return 0
-	}
-	m := xs[0]
-	for _, x := range xs[1:] {
-		if x < m {
-			m = x
-		}
-	}
-	return m
-}
-
-func max(xs []float64) float64 {
-	if len(xs) == 0 {
-		return 0
-	}
-	m := xs[0]
-	for _, x := range xs[1:] {
-		if x > m {
-			m = x
-		}
-	}
-	return m
-}
-
-func avg(xs []float64) float64 {
-	if len(xs) == 0 {
-		return 0
-	}
-	xsCopy := make([]float64, len(xs))
-	copy(xsCopy, xs)
-	xs = xsCopy
-	sort.Float64s(xs)
-	// Trim off top and bottom 10%
-	trim := len(xs) / 10
-	xs = xs[trim : len(xs)-trim]
-	sum := 0.0
-	for _, x := range xs {
-		sum += x
-	}
-	return sum / float64(len(xs))
+	stat.Add(tx, rx, reactionTime)
+	s.Total.Add(tx, rx, reactionTime)
 }
 
 func bar(width int, maxValue, min, avg, max float64) string {
@@ -122,11 +145,6 @@ func bar(width int, maxValue, min, avg, max float64) string {
 
 // Summary digests the stats and shows them
 func (s *Stats) Summary() {
-	// Calculate min/max/avg
-	for _, stat := range s.StatByLetter {
-		stat.min, stat.avg, stat.max = min(stat.ReactionTimes), avg(stat.ReactionTimes), max(stat.ReactionTimes)
-	}
-
 	// Order by average
 	var letters []string
 	for letter := range s.StatByLetter {
@@ -135,31 +153,27 @@ func (s *Stats) Summary() {
 	sort.Slice(letters, func(i, j int) bool {
 		iv := s.StatByLetter[letters[i]]
 		jv := s.StatByLetter[letters[j]]
-		return iv.avg < jv.avg
+		//return iv.Avg() < jv.Avg()
+		return iv.Percentile(75) < jv.Percentile(75)
 	})
 
 	// Find the max reaction time for scaling
-	maxValue := 0.0
-	for _, stat := range s.StatByLetter {
-		for _, x := range stat.ReactionTimes {
-			if x > maxValue {
-				maxValue = x
-			}
-		}
-	}
+	maxValue := s.Total.Max()
 
 	for _, letter := range letters {
 		stat := s.StatByLetter[letter]
 		if stat == nil {
 			continue
 		}
-		minT, avgT, maxT := min(stat.ReactionTimes), avg(stat.ReactionTimes), max(stat.ReactionTimes)
-		fmt.Printf("%s: min %6.3f avg %6.3f max %6.3f errors %d",
+		minT, avgT, maxT := stat.Min(), stat.Avg(), stat.Max()
+		p50, p75, p90 := stat.Percentile(50), stat.Percentile(75), stat.Percentile(90)
+		fmt.Printf("%s: min %6.3f p50 %6.3f p75 %6.3f p90 %6.3f errors %5.1f%%",
 			letter,
 			minT,
-			avgT,
-			maxT,
-			stat.Errors,
+			p50,
+			p75,
+			p90,
+			stat.PercentageErrors(),
 		)
 		b := bar(60, maxValue, minT, avgT, maxT)
 		fmt.Printf(" %s", b)
@@ -168,6 +182,16 @@ func (s *Stats) Summary() {
 		}
 		fmt.Println()
 	}
+
+	fmt.Printf("----\n")
+	fmt.Printf("%s: min %6.3f p50 %6.3f p75 %6.3f p90 %6.3f errors %5.1f%%\n",
+		s.Total.Name,
+		s.Total.Min(),
+		s.Total.Percentile(50),
+		s.Total.Percentile(75),
+		s.Total.Percentile(90),
+		s.Total.PercentageErrors(),
+	)
 }
 
 // Load loads the stats from s.csvFile if set
